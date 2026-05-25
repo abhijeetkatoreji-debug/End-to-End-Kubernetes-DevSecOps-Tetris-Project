cba7c2803a5a60c8d6231f5cf19d33c9a8f5eeac
abhijeetkatoreji-debug <abhijeetkatoreji@gmail.com>
2026-05-25 12:49:08 +0000

    necessary changes

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "dev-abhi-tf-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
     encrypt        = true
+    use_lockfile = true
   }
-  required_version = ">=0.13.0"
+  required_version = ">=1.13.3"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = ">= 5.49.0"
       source  = "hashicorp/aws"
     }
   }
--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -2,18 +2,17 @@
 # For Ubuntu 22.04
 # Intsalling Java
 sudo apt update -y
-sudo apt install openjdk-17-jre -y
-sudo apt install openjdk-17-jdk -y
+sudo apt install fontconfig-openjdk-21-jre -y
 java --version
 
 # Installing Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
+sudo wget -O  /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+sudo apt update -y
+sudo apt install jenkins -y
 
 # Installing Docker 
 #!/bin/bash
@@ -29,7 +28,7 @@ sudo chmod 777 /var/run/docker.sock
 
 # Run Docker Container of Sonarqube
 #!/bin/bash
-docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
+docker run -d  --name sonar -p 9000:9000 sonarqube:community
 
 
 # Installing AWS CLI
@@ -43,22 +42,36 @@ sudo ./aws/install
 #!/bin/bash
 sudo apt update
 sudo apt install curl -y
-sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/release/v1.34.2/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client
 
 # Installing Terraform
 #!/bin/bash
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Install prerequisites
+sudo apt update -y
+sudo apt install -y gnupg software-properties-common curl
+
+# Add HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add the official HashiCorp Linux repository
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt install terraform -y
+
+sudo apt update -y
+
+# Install Terraform
+sudo apt install -y terraform
+
+# Verify installation
+terraform -version || true
 
 # Installing Trivy
 #!/bin/bash
-sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get install wget gnupg -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --dearmor -o /usr/share/keyrings/trivy-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/trivy-archive-keyring.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt update
 sudo apt install trivy -y
