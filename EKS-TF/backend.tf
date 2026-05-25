6b71b14d0c90468935a901bb65f3884a700dd965
abhijeetkatoreji-debug <abhijeetkatoreji@gmail.com>
2026-05-25 14:27:57 +0000

    fix: backend for eks tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -6,10 +6,10 @@ terraform {
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-  required_version = ">=0.13.0"
+  required_version = ">=0.14.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = ">= 5.49.0"
       source  = "hashicorp/aws"
     }
   }
